fee4fcf66d1248456c9741ac6c489a0290559ecc
devlikepro <waha@devlike.pro>
2025-03-03 10:05:44 +0700

    [WAHA] HQ link preview

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -1122,7 +1122,8 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 								...(httpRequestOptions || {})
 							},
 							logger,
-							uploadImage: generateHighQualityLinkPreview ? waUploadToServer : undefined
+							uploadImage:
+								generateHighQualityLinkPreview || options.linkPreviewHighQuality ? waUploadToServer : undefined
 						}),
 					//TODO: CACHE
 					getProfilePicUrl: sock.profilePictureUrl,
