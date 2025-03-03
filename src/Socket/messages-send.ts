0b7922333a849336e39f087f960c6ec08ac9d912
devlikepro <waha@devlike.pro>
2025-03-03 10:05:44 +0700

    [WAHA] HQ link preview

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -1281,7 +1281,8 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 								...(axiosOptions || {})
 							},
 							logger,
-							uploadImage: generateHighQualityLinkPreview ? waUploadToServer : undefined
+							uploadImage:
+								generateHighQualityLinkPreview || options.linkPreviewHighQuality ? waUploadToServer : undefined
 						}),
 					//TODO: CACHE
 					getProfilePicUrl: sock.profilePictureUrl,
