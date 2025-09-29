9fea6b355c861fb016aad2d6930ecb880f9c63f4
devlikepro <waha@devlike.pro>
2025-09-29 15:14:41 +0700

    [WAHA] Fix sending poll to newsletter

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -49,6 +49,7 @@ import {
 	isJidHostedLidUser,
 	isJidHostedPnUser,
 	isLidUser,
+	isJidNewsletter,
 	isPnUser,
 	jidDecode,
 	jidEncode,
@@ -633,6 +634,9 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 					},
 					content: binaryNodeContent
 				}
+				if (additionalNodes && additionalNodes.length > 0) {
+					;(stanza.content as BinaryNode[]).push(...additionalNodes)
+				}
 				logger.debug({ msgId }, `sending newsletter message to ${jid}`)
 				await sendNode(stanza)
 				return
@@ -1168,7 +1172,8 @@ export const makeMessagesSocket = (config: SocketConfig) => {
 					additionalNodes.push({
 						tag: 'meta',
 						attrs: {
-							polltype: 'creation'
+							polltype: 'creation',
+							contenttype: isJidNewsletter(jid) ? 'text' : undefined,
 						}
 					} as BinaryNode)
 				} else if (isEventMsg) {
